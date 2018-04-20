ec6bac358d10e7f2508e43c18b2a5b9f8c241729
Matheus Santos <matheusmstos@yahoo.com.br>
2018-04-20 16:21:55 -0300

    regi

--- Projeto/Regs.v
+++ Projeto/Regs.v
@@ -16,4 +16,22 @@ module regn(R, Rin, Clock, Q)
 	end
 endmodule
 
-module regi()
+module regi(R, Rin, Clock, Q)
+	input [8:0] R;
+	input	Rin;
+	input Clock;
+	
+	output reg [8:0]Q;
+	
+	initial begin 
+		Q = 9'b0;
+	end
+	
+	always @(posedge Clock) begin
+		if(Rin) begin
+			Q <= R;
+		end
+	end
+endmodule
+
+endmodule
